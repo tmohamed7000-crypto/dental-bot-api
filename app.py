e3a8de77321c8d7215038b5e592ff5b2b7bec054
Abdelfattah_Mohamed <t.mohamed7000@gmail.com>
2026-05-02 06:08:03 +0300

    fix port + run

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "HELLO", 200
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# شغل السيرفر مباشرة بدون شرط
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
